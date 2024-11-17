872df47f67ccc382750773bed72600f23a1cf6f3
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-11-16 21:17:39 -0500

    Initial fbo fragment shaders

--- src/glsl/fragmentShaderPosition.js
+++ src/glsl/fragmentShaderPosition.js
@@ -1,10 +1,18 @@
 export const fragmentShaderPosition = /* glsl */ `
+    uniform sampler2D dtPosition;
     uniform sampler2D dtVelocity;
-    uniform float uTime;
+    uniform float time;
 
     void main() {
-        vec3 color = vec3(0.);
+        // vec3 color = vec3(0.);
+        vec2 uv = gl_FragCoord.xy / resolution.xy;
+        // vec4 tmpPos = texture2D(dtPosition, uv);
+        // vec3 position = tmpPos.xyz;
+        vec3 position = texture2D(dtPosition, uv).xyz;
+        vec3 velocity = texture2D(dtVelocity, uv).xyz;
 
-        gl_FragColor = vec4(color, 1.);
+        gl_FragColor = vec4(position + velocity, 1.);
+
+        // gl_FragColor = vec4(color, 1.);
     }
 `;
--- src/glsl/fragmentShaderVelocity.js
+++ src/glsl/fragmentShaderVelocity.js
@@ -1,12 +1,16 @@
 export const fragmentShaderVelocity = /* glsl */ `
+    uniform sampler2D dtPosition;
     uniform sampler2D dtVelocity;
     uniform float time;
 
     void main() {
+        vec2 uv = gl_FragCoord.xy / resolution.xy;
 
-        vec3 color = vec3(0.);
+        // vec3 color = vec3(0.);
+        vec3 position = texture2D( dtPosition, uv).xyz;
+        vec3 velocity = texture2D( dtVelocity, uv).xyz;
 
-        gl_FragColor = vec4(color, 1.);
+        gl_FragColor = vec4(velocity, 1.0);
 
     }
 `;
