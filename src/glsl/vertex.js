35a02942ed46c7c139bc67da608be89387041d8f
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-11-17 20:14:31 -0500

    Add uPositions

--- src/glsl/vertex.js
+++ src/glsl/vertex.js
@@ -1,13 +1,16 @@
 export const vertex = /* glsl */ `
-    uniform float uProgress;
+    uniform sampler2D uPositions;
     uniform float uTime;
     attribute vec2 reference;
     varying vec2 vUv;
 
     void main() {
-        vUv = reference;
+        vUv = uv;
+        vec3 pos = texture2D(uPositions, reference).xyz;
 
-        vec4 mvPosition = uProgress * modelViewMatrix * vec4( position, 1. );
+        // vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
+        // vec4 mvPosition = modelViewMatrix * vec4( reference, 1., 1. );
+        vec4 mvPosition = modelViewMatrix * vec4( pos, 1. );
         // start with big particles; give them some perspective
         gl_PointSize = 25. * ( 1. / - mvPosition.z );
         gl_Position = projectionMatrix * mvPosition;
