97acd3076e1dea5fc4eeaba7cc27586350c28506
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-11-18 20:39:52 -0500

    Pull data-texture filling functions into utility.

---
+++ src/utils/fill-position-texture.js
@@ -0,0 +1,22 @@
+export function fillPositionTexture(dataTexture) {
+    const theArray = dataTexture.image.data;
+
+    for (let k = 0, kl = theArray.length; k < kl; k += 4) {
+        theArray[k + 0] = 2 * (Math.random() - 0.5);
+        theArray[k + 1] = 2 * (Math.random() - 0.5);
+        theArray[k + 2] = 0;
+        theArray[k + 3] = 1;
+    }
+}
+
+export function fillPositionTextureFromPoints(dataTexture, points) {
+    const theArray = dataTexture.image.data;
+
+    for (let k = 0, kl = theArray.length; k < kl; k += 4) {
+        const pointIdx = k / 4;
+        theArray[k + 0] = points[pointIdx][0];
+        theArray[k + 1] = points[pointIdx][1];
+        theArray[k + 2] = 0;
+        theArray[k + 3] = 1;
+    }
+}
